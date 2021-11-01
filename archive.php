6528989e043615045766d6824beebc3f175cd5f5
Jake Howell <jake.howell302@gmail.com>
2021-11-01 12:02:12 -0500

    page additions

--- archive.php
+++ archive.php
@@ -1,37 +1,25 @@
 <?php
-/**
- * The template for displaying archive pages
- *
- * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
- *
- * @package WordPress
- * @subpackage Twenty_Twenty_One
- * @since Twenty Twenty-One 1.0
- */
-
 get_header();
-
-$description = get_the_archive_description();
 ?>
 
-<?php if ( have_posts() ) : ?>
+		<article class="content px-3 py-5 p-md-5">
 
-	<header class="page-header alignwide">
-		<?php the_archive_title( '<h1 class="page-title">', '</h1>' ); ?>
-		<?php if ( $description ) : ?>
-			<div class="archive-description"><?php echo wp_kses_post( wpautop( $description ) ); ?></div>
-		<?php endif; ?>
-	</header><!-- .page-header -->
+        <?php
+            if( have_posts() ){
 
-	<?php while ( have_posts() ) : ?>
-		<?php the_post(); ?>
-		<?php get_template_part( 'template-parts/content/content', get_theme_mod( 'display_excerpt_or_full_post', 'excerpt' ) ); ?>
-	<?php endwhile; ?>
+                while( have_posts() ){
 
-	<?php twenty_twenty_one_the_posts_navigation(); ?>
+                    the_post();
+                    
+                    get_template_part('template-parts/content', 'archive');
+                }
+            }
+	    ?>
+        
+	    </article>
+	   
 
-<?php else : ?>
-	<?php get_template_part( 'template-parts/content/content-none' ); ?>
-<?php endif; ?>
+<?php
+get_footer();
+?>
 
-<?php get_footer(); ?>
